b27bfec44daaa78cdf29c7275e84527440fbd14a
William Santana <williamandrade1058@gmail.com>
2024-05-30 15:27:35 -0300

    Menu de interface usuário

---
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -0,0 +1,20 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.service.ConsumoAPI;
+
+import java.util.Scanner;
+
+public class Principal {
+    private Scanner leitura = new Scanner(System.in);
+    private ConsumoAPI consumo = new ConsumoAPI();
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=c93a766";
+
+    public void exibiMenu() {
+        System.out.println("Digite o nome da série: ");
+        var nomeSerie = leitura.nextLine();
+        var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+        ;
+        // "https://www.omdbapi.com/?t=vikings&season=" + i + "&apikey=c93a766"
+    }
+}
